784d9a6adf417320d12d0d1b392e69dcd3e57862
Ronil Patel <rn373235@dal.ca>
2023-06-17 21:00:52 -0300

    Updated the container logic

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 
 
 def validate_file_contents(file_content):
+    # print(file_content[0][0])
+    # print(file_content[0][1])
     if file_content[0][0] != "product" or file_content[0][1] != "amount":
-        return True
+        raise
 
 
 @app.route('/calculate_sum', methods=['POST'])
@@ -22,29 +24,31 @@ def calculate():
 
     file_lines = []
 
-    try:
-        with open("/Ronil_PV_dir/" + payload.get("file"), "r") as file_obj:
-            file_lines = file_obj.readlines()
-            logging.error(file_lines)
-            print(f"Read lines are: {file_lines}")
-    except:
-        return json.loads(json.dumps({
-            "file": payload.get("file"),
-            "error": "Input file not able to read."
-        }))
+    # try:
+    #     with open("/Ronil_PV_dir/" + payload.get("file"), "r") as file_obj:
+    #         file_lines = file_obj.readlines()
+    #         logging.error(file_lines)
+    #         print(f"Read lines are: {file_lines}")
+    # except:
+    #     return json.loads(json.dumps({
+    #         "file": payload.get("file"),
+    #         "error": "Input file not able to read."
+    #     }))
 
     try:
-        # file_obj = open("/Ronil_PV_dir/" + payload.get("file"))
+        # with open("/home/" + payload.get("file"), "w") as file_obj:
+        #     file_obj.write(payload.get("data"))
+
+        file_obj = open("/Ronil_PV_dir/" + payload.get("file"), "r")
+        file_lines = file_obj.readlines()
+        print(f"file lines: {file_lines}")
+
         data_content = [i.replace(" ", "").strip().split(",") for i in file_lines]
         print(f"Data contents are: {data_content}")
-        logging.error(data_content)
         result = validate_file_contents(data_content)
-        logging.error(f"The result of validation is: {result}")
-        if not result:
-            raise
+
 
         data_content = data_content[1:]
-        logging.error(f'data content is: {data_content}')
 
         for row in data_content:
             if row[0] == payload['product']:
